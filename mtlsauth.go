5f70e28f912b4604c01c6b6f7efe74fc23d2533a
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-02-10 06:10:34 +0100

    Change json filed name

--- mtlsauth.go
+++ mtlsauth.go
@@ -25,7 +25,7 @@ type Config struct {
 	Region             string `json:"region"`
 	Environment        string `json:"environment"`
 	ServicePrivateKey  string `json:"service_private_key"`
-	ServiceUser        string `json:"service_user"`
+	ServiceIdentity    string `json:"service_identity"`
 	MTLSHeader         string `json:"mtls_header"`
 	SerialHeader       string `json:"serial_header"`
 	GetDeviceEndpoint  string `json:"get_device_endpoint"`
@@ -82,7 +82,7 @@ func (conf *Config) Access(kong *pdk.PDK) {
 			if conf.err == nil {
 				err := conf.serviceClient.ServiceLogin(iam.Service{
 					PrivateKey: conf.ServicePrivateKey,
-					ServiceID:  conf.ServiceUser,
+					ServiceID:  conf.ServiceIdentity,
 				})
 				if err != nil {
 					conf.err = err
@@ -101,7 +101,7 @@ func (conf *Config) Access(kong *pdk.PDK) {
 			conf.err = err
 		}
 	})
-	_ = kong.ServiceRequest.SetHeader("X-Service-ID", conf.ServiceUser)
+	_ = kong.ServiceRequest.SetHeader("X-Service-ID", conf.ServiceIdentity)
 
 	if conf.err != nil {
 		_ = kong.ServiceRequest.SetHeader("X-Plugin-Error", fmt.Sprintf("init failed: %v", conf.err))
