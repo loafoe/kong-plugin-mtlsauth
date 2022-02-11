530c46bfa1d2ad490079e9985ab1a57e610ccfcb
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-02-11 06:40:13 +0100

    Do not bail on negative expiry

--- mtlsauth.go
+++ mtlsauth.go
@@ -71,7 +71,7 @@ func New() interface{} {
 func (conf *Config) Access(kong *pdk.PDK) {
 	conf.doOnce.Do(func() {
 		conf.verifier, conf.err = signer.New(conf.SharedKey, conf.SecretKey)
-		conf.cache = cache.New(30*time.Minute, 60*time.Minute)
+		conf.cache = cache.New(30*time.Minute, 30*time.Minute)
 		if conf.err == nil {
 			conf.serviceClient, conf.err = iam.NewClient(nil, &iam.Config{
 				Region:      conf.Region,
@@ -149,8 +149,7 @@ func (conf *Config) Access(kong *pdk.PDK) {
 	expiresIn := time.Until(tokenResponse.ExpiresAt) / time.Second
 	if expiresIn <= 0 {
 		conf.cache.Delete(key)
-		_ = kong.ServiceRequest.SetHeader("X-Token-Error", "negative expiry")
-		return
+		_ = kong.ServiceRequest.SetHeader("X-Token-Warning", "negative expiry")
 	}
 	_ = kong.ServiceRequest.SetHeader("Authorization", "Bearer "+tokenResponse.AccessToken)
 	_ = kong.ServiceRequest.SetHeader("X-Token-Expires", fmt.Sprintf("%d", expiresIn))
