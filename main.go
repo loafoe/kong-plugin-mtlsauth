210c1e36179f1f1b4944c685ddc33b08f994579a
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-06-20 17:41:32 +0200

    Rename mtlsauth package to auth to prevent binary name clash

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/Kong/go-pdk/server"
-	"github.com/loafoe/kong-plugin-mtlsauth/mtlsauth"
+	"github.com/loafoe/kong-plugin-mtlsauth/auth"
 )
 
 func main() {
-	_ = server.StartServer(mtlsauth.New, "0.1", 1000)
+	_ = server.StartServer(auth.New, "0.1", 1000)
 }
