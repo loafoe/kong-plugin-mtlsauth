0e43dee5dcf9cd2959af9b7e27e7f92e4696f31b
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-06-20 17:10:35 +0200

    Rewrite plugin to fetch token from IIC IAM Proxy

--- mtlsauth/types.go
+++ mtlsauth/types.go
@@ -8,6 +8,6 @@ type mapperResponse struct {
 	AccessToken  string    `json:"access_token"`
 	RefreshToken string    `json:"refresh_token"`
 	TokenType    string    `json:"token_type"`
-	ExpiresIn    int       `json:"expires_in"`
+	ExpiresIn    int64     `json:"expires_in"`
 	ExpiresAt    time.Time `json:"-"`
 }
